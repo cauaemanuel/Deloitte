d5a589f3ab72c20c943ff02f2d423a764b38376d
bootcamp04 <bootcamp04@deloitte.com>
2025-05-05 17:54:30 -0300

    add: new class pessoa

---
+++ src/main/java/exercice1/Pessoa.java
@@ -0,0 +1,35 @@
+package exercice1;
+
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public boolean deMaior(){
+        if(idade >= 18){
+            return true;
+        }
+        return false;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
